d05e35a4eecf8bfd44520ff58f6b1f563ba1a6c4
saito-mk <saito-mk@magaseek.com>
2023-12-25 17:38:04 +0900

    レスポンシビリティ改善

--- chatbot/server.js
+++ chatbot/server.js
@@ -1,36 +1,39 @@
-const API_KEY = 'YOUR API KEY';
+const API_KEY = 'APIKEY';
 
 const URL = "https://api.openai.com/v1/chat/completions";
 
-async function reply() {
-    try {
-        const text = document.getElementById("request_text").value;
-        if (!text) {
-            // ユーザーからの入力が空白の場合のバリデーション
-            console.log("ユーザーからの入力が空です。");
-            return;
-        }
+function reply() {
+    var text = document.getElementById("request_text").value;
 
-        const response = await axios.post(
-            URL,
-            {
-                "model": "gpt-3.5-turbo",
-                "messages": [
-                    { "role": "user", "content": text }
-                ]
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${API_KEY}`,
-                },
-            }
-        );
-
-        const chatgpt_response = response.data.choices[0].message.content;
-        $("#response_text").val(chatgpt_response);
-    } catch (error) {
-        console.log("エラーが発生しました:", error);
-        // ユーザーにエラーを通知する方法を追加
+    // メッセージが空の場合に警告メッセージを表示
+    if (text.trim() === "") {
+        document.getElementById("response_text").value = "メッセージを入力してください";
+        return; // 処理を中断
     }
+    document.getElementById("response_text").value = "応答を取得しています..."; // 処理中の状態を表示
+    async function getResponse() {
+        try {
+            const response = await axios.post(
+                URL,
+                {
+                    "model": "gpt-3.5-turbo",
+                    "messages": [
+                        { "role": "user", "content": text }
+                    ]
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${API_KEY}`,
+                    },
+                }
+            );
+            var chatgpt_response = response.data.choices[0].message.content;
+            $("#response_text").val(chatgpt_response);
+        } catch (error) {
+            console.log(error);
+            document.getElementById("response_text").value = "エラーが発生しました。もう一度試してください。";
+        }
+    }
+    getResponse();
 }
