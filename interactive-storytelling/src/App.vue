90975d3d8f6090250554f5556384de4c75c54bdb
saito-mk <saito-mk@magaseek.com>
2023-12-27 17:50:42 +0900

    first commit

---
+++ interactive-storytelling/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <story-section :section="currentSection" @section-changed="updateSection" />
+  </div>
+</template>
+
+<script>
+import StorySection from './components/StorySection.vue';
+
+export default {
+  components: {
+    StorySection
+  },
+  data() {
+    return {
+      currentSectionId: 1,
+      story: {
+        1: {
+          title: "章1",
+          content: "物語の始まり。主人公は運命の旅に出る決意をします。",
+          options: [
+            { text: "旅に出る", nextSection: 2 },
+            { text: "家に留まる", nextSection: 3 }
+          ]
+        },
+        2: {
+          title: "章2",
+          content: "主人公は冒険の旅に出ます。",
+          options: [
+            { text: "森を探検する", nextSection: 4 },
+            { text: "町を探検する", nextSection: 5 }
+          ]
+        },
+        3: {
+          title: "家に留まる",
+          content: "主人公は家に留まり、平凡な日々を送ります。",
+          options: [
+            { text: "再考する", nextSection: 1 }
+          ]
+        },
+        // 他のセクションもここに追加...
+      }
+    };
+  },
+  computed: {
+    currentSection() {
+      return this.story[this.currentSectionId];
+    }
+  },
+  methods: {
+    updateSection(sectionId) {
+      this.currentSectionId = sectionId;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ interactive-storytelling/src/components/StorySection.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="story-section">
+    <h2>{{ section.title }}</h2>
+    <p>{{ section.content }}</p>
+    <button v-for="option in section.options" :key="option.nextSection" @click="goToSection(option.nextSection)">
+      {{ option.text }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    section: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    goToSection(nextSectionId) {
+      this.$emit('section-changed', nextSectionId);
+    }
+  }
+}
+</script>
+
+<style>
+.story-section {
+  padding: 20px;
+  background-color: #f4f4f4;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+
+.story-section h2 {
+  color: #333;
+}
+
+.story-section p {
+  color: #666;
+}
+
+.story-section button {
+  background-color: #008CBA;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+
+.story-section button:hover {
+  background-color: #005f73;
+}
+</style>
